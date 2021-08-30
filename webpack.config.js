6833a97ad15877ffe9e0d6cbc22932533411f5ba
Ziad Ayman <ziad.ayman@itworx.com>
2021-08-30 11:26:39 +0200

    Adding fonts

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,11 @@ module.exports = {
 		rules: [
 			{
 				test: /\.s[ac]ss$/i,
-				use: [ 
-					{ loader: "style-loader" },
-					{ loader: "css-loader", options: { url: false } },
-					{ loader: "sass-loader" }
-				]
+				use: [ "style-loader", "css-loader", "sass-loader" ]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
 			}
 		]
 	},
